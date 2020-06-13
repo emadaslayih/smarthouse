64c02d8f7ca54f58ba3e4ed83ca121cb385d94ea
emadaslayih <emad-slayeh@windowslive.com>
2020-06-13 13:16:32 +0300

    aaa

---
+++ precache-manifest.37e1fe1b1457263b953415b152382ffd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3fb827d1edd62e2847f09d5f6a7b1d7",
+    "url": "/smarthouse/index.html"
+  },
+  {
+    "revision": "57f58b3d7f45ebdb5e31",
+    "url": "/smarthouse/static/css/2.2990c7a7.chunk.css"
+  },
+  {
+    "revision": "c77e503f88ea2ee06faa",
+    "url": "/smarthouse/static/css/main.60885808.chunk.css"
+  },
+  {
+    "revision": "57f58b3d7f45ebdb5e31",
+    "url": "/smarthouse/static/js/2.ccbdbdd7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/smarthouse/static/js/2.ccbdbdd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c77e503f88ea2ee06faa",
+    "url": "/smarthouse/static/js/main.ca382f71.chunk.js"
+  },
+  {
+    "revision": "52c19377f69af6e1af76",
+    "url": "/smarthouse/static/js/runtime-main.ff7cfaa9.js"
+  }
+]);
